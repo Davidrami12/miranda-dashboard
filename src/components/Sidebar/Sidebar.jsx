16b6e11297d83c124caa8586e4260dca26cd9e06
Davidrami12 <davidrami2000@gmail.com>
2023-06-27 11:13:13 +0200

    Redone sidebar to hide/show properly

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import styled from 'styled-components';
-import { FaGripVertical, FaKey, FaRegCalendarAlt, FaRegUser, FaPhoneAlt } from 'react-icons/fa';
-import logo from '../../assets/logo.jpg'
+//import LoguedUser from './LoguedUser';
+import Logo from '../../assets/logo.jpg'
+
+import {
+    NavContainer,
+    LogoContainer,
+    Navigation,
+    ArrowButton,
+    UserCard,
+    NavigationDescription,
+    NavigationRights,
+    NavigationAuthor,
+    Link
+} from "./SidebarStyled"
 
 import { BiKey } from "react-icons/bi";
 import { BsCalendarCheck } from "react-icons/bs";
@@ -11,100 +22,64 @@ import { AiFillHome, AiFillContacts, AiFillHeart } from "react-icons/ai";
 
 
 
-
-const SidebarContainer = styled.div`
-  /*position: fixed;
-  top: 0;
-  left: 0;
-  width: 345px;
-  height: 100%;
-  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-  background: lightgreen;
-  box-shadow: 13px 3px 40px #00000005;
-  opacity: 1;
-  //padding: 20px;
-  z-index: 99;*/
-  padding: 2rem;
-  background: lightgreen;
-  height: 100vh;
-
-  & > img {
-    width: 280px;
-  }
-`;
-
-const SidebarList = styled.ul`
-  margin-top: 2rem;
-  list-style-type: none;
-`;
-
-const SidebarListItem = styled.li`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  color: #799283;
-  font-size: 18px;
-  font: normal normal normal 18px/27px Poppins;
-  padding: 2rem 0rem 2rem 0rem;
-
-  & > a {
-    width: 100%;
-
-    &:hover{
-      color: #E23428;
-    }
-  }
-`;
-
-
 export const Sidebar = () => {
 
-  const [display, setDisplay] = useState(0);
+    /*let user = {
+        photo: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        name: "Sashenka Gouthier",
+        email: "sgouthier0@aboutads.info",
+    }*/
+
+    const [display, setDisplay] = useState(0);
     const location = useLocation();
 
     const displayMenu = () => {
         setDisplay(!display)
     }
-    
-  return (
-    <SidebarContainer>
-      <img src={logo} alt="Logo" />
-      <SidebarList>
-        
-        <SidebarListItem>
-          <FaGripVertical style={{width: '24px', height: '27px', cursor: 'pointer'}}/>
-          <NavLink to="/">Dashboard</NavLink>
-        </SidebarListItem>
 
-        <SidebarListItem>
-          <NavLink to="/bookings">
-            <div/>
-            <FaGripVertical className='icon'/>
-            <p>Bookings</p>
-          </NavLink>
-        </SidebarListItem>
+    return (
+        <NavContainer display={display ? "0px" : "345px" }>
+            <ArrowButton onClick={displayMenu}><FaArrowsAltH className='arrow' /></ArrowButton>
+            <LogoContainer>
+                <img src={Logo} />
+            </LogoContainer>
+            <Navigation>
+              <ul>
+                <NavLink to="/">
+                    <Link route="/" current={location.pathname}>
+                        <div /><AiFillHome className='icon' /><p>Dashboard</p>
+                    </Link>
+                </NavLink>
+                <NavLink to="/bookings">
+                    <Link route="/bookings" current={location.pathname}>
+                        <div /><BsCalendarCheck className='icon' /><p>Bookings</p>
+                    </Link>
+                </NavLink>
+                <NavLink to="/rooms">
+                    <Link route="/rooms" current={location.pathname}>
+                        <div /><BiKey className='icon' /><p>Rooms</p>
+                    </Link>
+                </NavLink>
+                <NavLink to="/contact">
+                    <Link route="/contact" current={location.pathname}>
+                        <div /><AiFillContacts className='icon' /><p>Contact</p>
+                    </Link>
+                </NavLink>
+                <NavLink to="/users">
+                    <Link route="/users" current={location.pathname}>
+                        <div /><FaUserFriends className='icon' /><p>Users</p>
+                    </Link>
+                </NavLink>
+              </ul>
+            </Navigation>
 
-        <SidebarListItem>
-          <NavLink to="/rooms">Rooms</NavLink>
-        </SidebarListItem>
+            {/*<UserCard>
+                <LoguedUser user={user}></LoguedUser>
+    </UserCard>*/}
 
-        <SidebarListItem>
-          <NavLink to="/users">Users</NavLink>
-        </SidebarListItem>
-
-        <SidebarListItem>
-          <NavLink to="/contact">Contact</NavLink>
-        </SidebarListItem>
-        
-      </SidebarList>
-
-
-
-      <div>
-        <p>Travl Hotel Admin Dashboard</p>
-        <p>&copy; 2023 All Rights Reserved</p>
-        <p>Made with ♥ by David R.</p>
-      </div>
-    </SidebarContainer>
-  )
+            <NavigationDescription>Travl Hotel Admin Dashboard</NavigationDescription>
+            <NavigationRights>&copy; 2023 All Rights Reserved</NavigationRights>
+            <NavigationAuthor>Made with ♥ by David R.</NavigationAuthor>
+        </NavContainer>
+    )
 }
---
+++ src/components/Sidebar/SidebarStyled.jsx
@@ -0,0 +1,151 @@
+import styled from 'styled-components';
+
+const NavContainer = styled.div`
+    width: ${props => props.display};
+    height: 100vh;
+    box-shadow: 13px 3px 40px #00000005;
+    background-color: #FFFFFF;
+    transition: width 0.3s;
+    overflow: visible;
+    position: relative;
+`;
+
+const ArrowButton = styled.button`
+    position: absolute;
+    right: -50px;
+    top: 45px;
+    background-color: transparent;
+    border: none;
+    width: 25px;
+    height: 25px;
+
+    &:focus{
+        outline: none;
+    }
+
+    .arrow{
+        width: 25px;
+        height: 25px;
+    }
+`;
+
+const UserCard = styled.div`
+    margin-left: 15%;
+    padding-bottom: 10px;
+    overflow: hidden;
+`;
+
+const LogoContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding-top: 30px;
+    overflow: hidden;
+
+    & > img {
+        width: 250px;
+    }
+`;
+
+const Navigation = styled.nav`
+overflow: hidden;
+    ul{
+        display: flex;
+        flex-direction: column;
+        list-style: none;
+        padding: 0;
+        a{
+            text-decoration: none;
+        }
+    }
+`;
+
+const Link = styled.li`
+    width: 70%;
+    display: flex;
+    height: 70px;
+    border-radius: 6px;
+    align-items: center;
+    gap: 20px;
+    transition: all 0.2s;
+    padding: 0.5rem 0rem 0.5rem 0rem;
+
+    &:hover p,
+    &:hover .icon{
+        color: #E23428;
+    }
+
+    div{
+        margin-right: 15%;
+        transition: background-color 0.2s;
+        width: 8px;
+        height: 100%;
+        background-color: ${props => {
+        if (props.route === props.current) {
+            return "#E23428";
+        } else {
+            return "#FFFFFF";
+        }
+    }};;
+        border-radius: 0 8px 8px 0;
+    }
+
+    p {
+        color: ${props => {
+        if (props.route === props.current) {
+            return "#E23428";
+        } else {
+            return "#799283";
+        }
+    }};
+        font: normal normal 600 18px/27px Poppins;
+        text-decoration: none;
+    }
+
+    .icon{
+        color: ${props => {
+        if (props.route === props.current) {
+            return "#E23428";
+        } else {
+            return "#799283";
+        }
+    }};
+        width: 27px;
+        height: 27px;
+    }
+`;
+
+const NavigationDescription = styled.p`
+    padding-left: 15%;
+    overflow: hidden;
+    font-family: var(--font-poppins);
+    font-weight: 600;
+    color: #212121;
+`;
+
+const NavigationRights = styled.p`
+    padding-left: 15%;
+    overflow: hidden;
+    color: #799283;
+    font-size: 14px;
+`;
+
+const NavigationAuthor = styled.p`
+    padding-left: 15%;
+    overflow: hidden;
+    color: #799283;
+    font-size: 14px;
+    margin-top: 30px;
+`;
+
+export {
+    NavContainer,
+    ArrowButton,
+    LogoContainer,
+    Navigation,
+    UserCard,
+    NavigationDescription,
+    NavigationRights,
+    NavigationAuthor,
+    Link
+};
