1102686bfdb2e0e921310df13249768e4bd9e755
Davidrami12 <davidrami2000@gmail.com>
2023-07-03 10:00:09 +0200

    Showing Rooms displaying JSON data

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Table } from '../../components/StyledComponents/Table'
+import contactData from '../../data/rooms.json'
 
 export const Rooms = () => {
+
+  const cols = ["photo", "room number", "ID", "bed type", "room floor", "facilities", "price", "status"];
+
+  const renderCell = (col, rowData) => {
+    if (col === 'status') {
+      return (
+        <span style={rowData[col] === 'available' ? {color: 'green'} : {color: 'red'}}>
+          {rowData[col].toUpperCase()}
+        </span>
+      )
+    } else {
+      return rowData[col];
+    }
+  }
+
   return (
-    <div>Rooms</div>
+    <div>
+      <div>Rooms</div>
+      <div>
+        <Table data={contactData} cols={cols} renderCell={renderCell} />
+      </div>
+    </div>
   )
 }
