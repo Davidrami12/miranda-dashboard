e202c2d9b77e8582d4f13f7524da83640210a63e
Davidrami12 <davidrami2000@gmail.com>
2023-07-03 15:29:44 +0200

    Displaying JSON data from Bookings

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { Table } from '../../components/StyledComponents/Table'
+import bookingsData from '../../data/bookings.json'
 
 export const Bookings = () => {
+
+  const cols = ["guest", "order date", "check in", "check out", "special request", "room type", "status"];
+
+  const renderCell = (cols, bookingsData) => {
+    if (cols === 'status') {
+      let color;
+      switch(bookingsData[cols]){
+        case 'booked':
+          color = 'green';
+          break;
+        case 'refund':
+          color = 'red';
+          break;
+        case 'in progress':
+          color = 'orange';
+          break;
+        default:
+          color = 'black';
+      }
+      return (
+        <span style={{color: color}}>
+          {bookingsData[cols]}
+        </span>
+      )
+    } else if (cols === 'special request') {
+      return (
+        <button onClick={() => handleRequest(bookingsData.requestId)}>
+          {bookingsData[cols]}
+        </button>
+      )
+    } else {
+      return bookingsData[cols];
+    }
+  }
+
+  const handleRequest = (requestId) => {
+    // Handle the click event here
+  }
+
   return (
-    <div>Bookings</div>
+    <div>
+      <div>Users</div>
+      <div>
+        <Table data={bookingsData} cols={cols} renderCell={renderCell} />
+      </div>
+    </div>
   )
 }
