20c8909e429b74a4fcb5d0e52800efe13815dade
Davidrami12 <davidrami2000@gmail.com>
2023-07-13 00:20:03 +0200

    Clean code Dashboard styles

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -67,66 +67,7 @@ export const Dashboard = () => {
         <p>Latest Reviews by Customers</p>
         <ReviewsSwiper></ReviewsSwiper>
       </Reviews>
+
     </DashboardContainer>
-
-
-
-    /* <DashboardContainer>
-      <KPI>
-        <KPIContainer>
-          <MdOutlineBedroomParent style={{width: 30, height: 30}} className='svg'/>
-          <div>
-            <h2>8,461</h2>
-            <p>New Booking</p>
-          </div>
-        </KPIContainer>
-
-        <KPIContainer>
-          <MdCalendarMonth style={{width: 30, height: 30}}/>
-          
-          <div>
-            <h2>963</h2>
-            <p>Scheduled Rooms</p>
-          </div>
-        </KPIContainer>
-
-        <KPIContainer>
-          <RiLogoutBoxRFill style={{width: 30, height: 30}}/>
-          
-          <div>
-            <h2>753</h2>
-            <p>Check In</p>
-          </div>
-        </KPIContainer>
-
-        <KPIContainer>
-          <RiLogoutBoxFill style={{width: 30, height: 30}}/>
-          
-          <div>
-            <h2>516</h2>
-            <p>Check Out</p>
-          </div>
-        </KPIContainer>
-      </KPI>
-      
-
-      <ReviewsBox>
-        <h1>Latest Review by Customers</h1>
-
-        <ReviewsContainer>
-
-          {reviews.map((review, index) => (
-            <Review key={index}>
-              <p>{review.comment}</p>
-              <p><b>{review.customer}</b></p>
-              <p>{review.email}</p>
-              <p>{review.phone}</p>
-            </Review>
-          ))}
-
-        </ReviewsContainer>
-      </ReviewsBox>
-
-    </DashboardContainer> */
   )
 }
