e6e180af4ce31925c966e4e3820d5f357c6b80af
Davidrami12 <davidrami2000@gmail.com>
2023-07-17 20:15:48 +0200

    Finish form to edit user

--- src/pages/Users/EditUser.jsx
+++ src/pages/Users/EditUser.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+// React
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useParams } from "react-router-dom";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, editUser } from "../../features/usersSlice";
+
+// Components
+import UserForm from "../../components/users/UserForm";
+import { Loader } from "../../components/styled/Loader";
 
 export const EditUser = () => {
-  return (
-    <div>EditUser</div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const { id } = params;
+  const { singleUser } = useSelector((state) => state.usersReducer);
+
+  const [currentUser, setCurrentUser] = useState(null);
+  const formTitle = "Edit current user";
+
+  useEffect(() => {
+    dispatch(getUser(Number(id)));
+  }, [dispatch, id]); // Removed singleUser from the dependency array
+
+  // Set currentUser whenever singleUser changes, but only if singleUser is not null
+  useEffect(() => {
+    if (singleUser) {
+      setCurrentUser(singleUser);
+    }
+  }, [singleUser]);
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    setCurrentUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setCurrentUser({});
+    navigate("/users");
+  };
+
+  const handleSubmit = () => {
+    dispatch(editUser(currentUser));
+    setCurrentUser({});
+    navigate("/users");
+  };
+  return !currentUser ? (
+    <Loader />
+  ) : (
+    <UserForm
+      formTitle={formTitle}
+      currentUser={currentUser}
+      handleInput={handleInput}
+      handleSubmit={handleSubmit}
+      handleCancel={handleCancel}
+    />
+  );
+};
